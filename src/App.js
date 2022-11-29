61d403f9b2906b73705a4d860d6cc4a723388879
Mrt Xee <mrtxee@gmail.com>
2022-11-29 17:08:28 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from 'react';
+import {Alert, Button, ButtonGroup, Spinner} from 'reactstrap';
+import AlertExample from "./components/ui/alert/alert";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    async function getHomes(){
+        console.log('getHomes()');
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        console.log(response.data);
+    }
+
+    const alertRef = useRef();
+    let isAlertExampleVisible=true;
+    const onLoadHomesClick = function() {
+        console.log('onLoadHomesClick');
+        getHomes().then(r => {
+            console.log('r is ok');
+        });
+        toggleAlert();
+    }
+    const [visible, setVisible] = useState(true);
+    const toggleAlert = function (){
+        if (visible)
+            setVisible(false);
+        else setVisible(true);
+    }
+    return (
+    <div>
+        <AlertExample
+            color="success"
+            visible = {visible}
+            toggleAlert = {toggleAlert}
+            msg = "lorem ipsum"
+        />
+        <p ref={alertRef}>the stuff is here</p>
+        <p>кнопки управления</p>
+        <ButtonGroup>
+            <Button color="warning" onClick={onLoadHomesClick}>
+                load homes
+            </Button>
+            <Button color="warning">
+                load rooms
+            </Button>
+            <Button color="warning">
+                load rooms devices
+            </Button>
+            <Button color="warning">
+                load all
+            </Button>
+        </ButtonGroup>
+        <p>лоадер</p>
+        <Spinner color="info">
+            Loading...
+        </Spinner>
+        <p>сообщения: данныне успешно загружены, ошибка</p>
+        <Alert color="warning" isOpen={visible} toggle={toggleAlert}>
+            hello
+        </Alert>
+        <p>список элементов пока линейный</p>
     </div>
   );
 }
