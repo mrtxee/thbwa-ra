250d5cd29d1d6cc62af38f7cad34e0746ef5e2c7
Mrt Xee <mrtxee@gmail.com>
2022-12-05 08:42:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,53 @@ import {Alert, Button, ButtonGroup, Spinner} from 'reactstrap';
 import AlertExample from "./components/ui/alert/alert";
 import axios from "axios";
 
+
 function App() {
-    async function getHomes(){
+    const [homes, setHomes] = useState([]);
+    const [rooms, setRooms] = useState([]);
+    const [devices, setDevices] = useState([]);
+
+
+    async function getHomes() {
         console.log('getHomes()');
-        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        const response = await axios.get('http://localhost:8000/api/v1.0/get_devices/4');
+        setHomes(response.data.data.homes)
+        setRooms(response.data.data.rooms)
+        setDevices(response.data.data.devices)
+
+
         console.log(response.data);
     }
 
+    // const onLoadHomes = function() {
+    //
+    //     getHomes().then(r => {
+    //         debugger;
+    //         setResp(r.data)
+    //     });
+    //
+    // }
+
+
+    const renderDeviceByType = (device) => {
+        let color = "red"
+        switch(device.category) {
+            case 'tgq':
+                color = "blue"
+                break
+        }
+
+        return (<li  style={{color: color}} key={device.uuid}>
+                {
+                    device.name
+                }
+            </li>
+        )
+    }
+
     const alertRef = useRef();
-    let isAlertExampleVisible=true;
-    const onLoadHomesClick = function() {
+    let isAlertExampleVisible = true;
+    const onLoadHomesClick = function () {
         console.log('onLoadHomesClick');
         getHomes().then(r => {
             console.log('r is ok');
@@ -20,46 +57,86 @@ function App() {
         toggleAlert();
     }
     const [visible, setVisible] = useState(true);
-    const toggleAlert = function (){
+    const toggleAlert = function () {
         if (visible)
             setVisible(false);
         else setVisible(true);
     }
     return (
-    <div>
-        <AlertExample
-            color="success"
-            visible = {visible}
-            toggleAlert = {toggleAlert}
-            msg = "lorem ipsum"
-        />
-        <p ref={alertRef}>the stuff is here</p>
-        <p>кнопки управления</p>
-        <ButtonGroup>
-            <Button color="warning" onClick={onLoadHomesClick}>
-                load homes
+        <div>
+            <AlertExample
+                color="success"
+                visible={visible}
+                toggleAlert={toggleAlert}
+                msg="lorem ipsum"
+            />
+            <Button color="warning" onClick={getHomes}>
+                test
             </Button>
-            <Button color="warning">
-                load rooms
-            </Button>
-            <Button color="warning">
-                load rooms devices
-            </Button>
-            <Button color="warning">
-                load all
-            </Button>
-        </ButtonGroup>
-        <p>лоадер</p>
-        <Spinner color="info">
-            Loading...
-        </Spinner>
-        <p>сообщения: данныне успешно загружены, ошибка</p>
-        <Alert color="warning" isOpen={visible} toggle={toggleAlert}>
-            hello
-        </Alert>
-        <p>список элементов пока линейный</p>
-    </div>
-  );
+
+            <div>
+                {homes.map(home =>
+                    <div key={home.home_id}>
+                        <p>{home.name}</p>
+                        <ul>
+                            {
+                                rooms.filter(r => r.home_id === home.home_id).map(room => <li key={room.room_id}>
+                                    {room.name}
+                                    <ul>
+                                        {
+                                            devices.filter(d => d.room_id === room.room_id).map(device =>
+                                                renderDeviceByType(device)
+                                            )
+                                        }
+                                    </ul>
+                                </li>)
+                            }
+                        </ul>
+
+                    </div>
+                )}
+            </div>
+        </div>
+        // <div>
+        //     <AlertExample
+        //         color="success"
+        //         visible = {visible}
+        //         toggleAlert = {toggleAlert}
+        //         msg = "lorem ipsum"
+        //     />
+        //     <p ref={alertRef}>the stuff is here</p>
+        //     <p>кнопки управления</p>
+        //     <ButtonGroup>
+        //         <Button color="warning" onClick={getHomes}>
+        //             test
+        //         </Button>
+        //         <Button color="warning" onClick={onLoadHomesClick}>
+        //             load homes
+        //         </Button>
+        //         <Button color="warning">
+        //             load rooms
+        //         </Button>
+        //         <Button color="warning">
+        //             load rooms devices
+        //         </Button>
+        //         <Button color="warning">
+        //             load all
+        //         </Button>
+        //     </ButtonGroup>
+        //     <p>лоадер</p>
+        //     <Spinner color="info">
+        //         Loading...
+        //     </Spinner>
+        //     <p>сообщения: данныне успешно загружены, ошибка</p>
+        //     <Alert color="warning" isOpen={visible} toggle={toggleAlert}>
+        //         hello
+        //     </Alert>
+        //     <p>список элементов пока линейный</p>
+        //     <div>
+        //         {homes.map(e => <span>e.home_id</span>)}
+        //     </div>
+        // </div>
+    );
 }
 
 export default App;
