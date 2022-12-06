222ec4c936e539c8853c1bd82ead57bb4f071e6a
Mrt Xee <mrtxee@gmail.com>
2022-12-06 20:22:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {Alert, Button, ButtonGroup, Spinner} from 'reactstrap';
 import axios from "axios";
 import HomeRoomsTabs from "./components/ui/HomeRoomsTabs";
@@ -10,12 +10,16 @@ function App() {
 
     //const [homes1, setHomes1] = useState([]);
 
+    useEffect(() => {
+        console.log("sdgsdgsdg")
+        //getDevices()
+    }, [])
+
     async function getDevices() {
         console.log('getDevices called');
-        const response = await axios.get('http://localhost:8001/api/v1.0/get_devices/2');
+        const response = await axios.get('http://localhost:8000/api/v1.0/get_devices/2');
         setHomes(response.data.data);
-        //setHomes(homes[0]);
-        console.log(homes[0]);
+        console.log(response.data.data[0]);
     }
     function homeChange(e){
         console.log('homeChange event says that current home_id is');
@@ -56,15 +60,7 @@ function App() {
                 )}
             </select>
             <HomeRoomsTabs home={cur_home}/>
-            {/*<RoomsTabsTesting key={'RoomsTabContainer'} rooms={rooms} />*/}
-            {/*<HomesTabsTesting key={'HomesTabContainer'} homes={homes} />*/}
-            {/*<div>*/}
-            {/*    {homes.map(home =>*/}
-            {/*        <HomeCt key={home.home_id} home={home}/>*/}
-            {/*    )}*/}
-            {/*</div>*/}
         </div>
-
     );
 }
 
