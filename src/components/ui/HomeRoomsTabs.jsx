4092b8843f77ae852e30cce80895f7e50abbcce7
mrtxee <mrtxee@bk.ru>
2023-02-14 13:30:46 +0300

    Update HomeRoomsTabs.jsx

--- src/components/ui/HomeRoomsTabs.jsx
+++ src/components/ui/HomeRoomsTabs.jsx
@@ -2,25 +2,30 @@ import React from 'react';
 import RoomCt from "./RoomCt";
 
 const HomeRoomsTabs = ({home, activeTabIndex, ...props}) => {
+    //console.log(home.rooms.length);
+
     return (
         <div className={"container-fluid m-0 p-0"}>
             {home && home.rooms && home.rooms.length
                 ?
                 <div>
-                    <ul className="nav nav-pills mb-1" id="pills-tab" role="tablist">
-                        {home.rooms && home.rooms.map((room, index) =>
-                            <li className="nav-item" role="presentation" key={"li"+room.room_id}>
-                                <button type="button" role="tab" data-bs-toggle="pill"
-                                        className={`nav-link${index === activeTabIndex ? ' active': ''}`}
-                                        aria-selected={`${index === activeTabIndex ? true: false}`}
-                                        id={"pills-"+room.room_id+"-tab"}
-                                        data-bs-target={"#pills-"+room.room_id}
-                                        aria-controls={"pills-"+room.room_id}
-                                >{room.name}
-                                </button>
-                            </li>
-                        )}
-                    </ul>
+                    {home.rooms.length > 1 &&
+                        <ul className="nav nav-pills mb-1" id="pills-tab" role="tablist">
+                            {home.rooms && home.rooms.map((room, index) =>
+                                <li className="nav-item" role="presentation" key={"li" + room.room_id}>
+                                    <button type="button" role="tab" data-bs-toggle="pill"
+                                            className={`nav-link${index === activeTabIndex ? ' active' : ''}`}
+                                            aria-selected={`${index === activeTabIndex ? true : false}`}
+                                            id={"pills-" + room.room_id + "-tab"}
+                                            data-bs-target={"#pills-" + room.room_id}
+                                            aria-controls={"pills-" + room.room_id}
+                                    >{room.name}
+                                    </button>
+                                </li>
+                            )}
+                        </ul>
+                    }
+
                     <div className="tab-content" id="pills-tabContent" style={{margin: "0rem -0.75rem"}}>
                         {home.rooms.map((room, index) =>
                             // <div className="tab-pane fade show active">...</div>
