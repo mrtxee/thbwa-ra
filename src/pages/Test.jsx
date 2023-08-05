1ad12bb5e2ff1483e0123d79e0f42c8f71d324ef
Mrt Xee <mrtxee@gmail.com>
2023-08-05 19:35:23 +0300

    google oath2 testunit added

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,8 +1,10 @@
 import React, {useState} from 'react';
 import axios from "axios";
+import {GoogleLogin, GoogleOAuthProvider} from "@react-oauth/google";
+import jwtDecode from 'jwt-decode'
 
 const Test = () => {
-
+    const CLIENT_ID_GOOGLE = "93483542407-ckrg8q5q527dmcd62ptg0am5j9jhvesb.apps.googleusercontent.com";
     const [userContext, setUserContext] = useState([]);
     async function getUserContext() {
         console.log('getUserContext');
@@ -25,6 +27,27 @@ const Test = () => {
     }
 
     return (<div>
+        <h1>GoogleLogin</h1>
+        <GoogleOAuthProvider clientId={CLIENT_ID_GOOGLE}>
+            <GoogleLogin
+                onSuccess={credentialResponse => {
+                    if (credentialResponse.credential != null) {
+                        const USER_CREDENTIAL = jwtDecode(credentialResponse.credential);
+                        console.log(USER_CREDENTIAL);
+                    }
+                }}
+                onError={() => {
+                    console.log('Login Failed');
+                }}
+                type = {"icon"}
+                //theme = {"filled_black"} //outline, filled_blue, filled_black
+                width = {280}
+                auto_select = {false}
+                ux_mode = {"popup"}
+                nonce = {"jthbwawt"}
+                //useOneTap
+            />
+        </GoogleOAuthProvider>
         <h1>test</h1>
         <p>console:</p>
         <samp className={"m-4"}>{JSON.stringify(userContext, null, 2)}</samp>
